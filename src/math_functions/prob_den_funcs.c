eaca5ee7f824c24fdd21b902c710d7a0b469f27b
Griger5 <griger04@gmail.com>
2024-08-01 13:10:02 +0200

    Fixed memory leak

--- src/math_functions/prob_den_funcs.c
+++ src/math_functions/prob_den_funcs.c
@@ -15,40 +15,27 @@ double multi_var_normal_pdf(const Matrix mean_vector, const Matrix covariance_ma
     if (mean_vector.cols != input_vector.cols || input_vector.cols != covariance_matrix.rows) return NAN;
 
     size_t vec_length = mean_vector.cols;
-    printf("vec_length: %ld\n", vec_length);
     double covar_determinant = determinant(covariance_matrix);
-    printf("covar_determinant: %f\n", covar_determinant);
 
     double normalization_factor = 1/sqrt(pow(2*M_PI, (double)vec_length) * covar_determinant);
-    printf("normalization_factor: %f\n", normalization_factor);
 
     Matrix inverse_covar = invert_matrix(covariance_matrix);
-    printf("inverse_covar:\n");
-    print_matrix(inverse_covar);
-    Matrix mean_input_diff = matrix_subtract_elements(input_vector, mean_vector);
-    printf("mean_input_diff:\n");
-    print_matrix(mean_input_diff);
 
-    Matrix invcov_multi_diff = matrix_multiply(mean_input_diff, inverse_covar);
-    printf("first multiplication:\n");
-    print_matrix(invcov_multi_diff);
-    invcov_multi_diff = matrix_multiply_elements(invcov_multi_diff, mean_input_diff);
-    printf("second multiplication:\n");
-    print_matrix(invcov_multi_diff);
+    Matrix mean_input_diff = matrix_subtract_elements(input_vector, mean_vector);
+
+    Matrix invcov_multi_diff = matrix_multiply(mean_input_diff, inverse_covar);    
+    Matrix invcov_multi_diff2 = matrix_multiply_elements(invcov_multi_diff, mean_input_diff);
 
     double sum = sum_row(invcov_multi_diff, 0);
-    printf("sum: %f\n", sum);
 
     double exponent = -0.5 * sum;
-    printf("exponent: %f\n", exponent);
 
     double value = pow(M_E, exponent);
-    printf("value: %f\n", value);
-    printf("final value: %.20f\n", value*normalization_factor);
 
     free_matrix(&inverse_covar);
     free_matrix(&mean_input_diff);
     free_matrix(&invcov_multi_diff);
+    free_matrix(&invcov_multi_diff2);
 
     return value*normalization_factor;
 }
