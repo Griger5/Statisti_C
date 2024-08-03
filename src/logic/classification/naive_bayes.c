2340f31e7d686844f306b61d1d431a7b1c5554cd
Griger5 <griger04@gmail.com>
2024-08-03 14:22:58 +0200

    Improved naive_bayes_classify_dataset

--- src/logic/classification/naive_bayes.c
+++ src/logic/classification/naive_bayes.c
@@ -72,10 +72,6 @@ int naive_bayes_classify(const Matrix input_vector, const Matrix *parameter_key)
 void naive_bayes_classify_dataset(DataSet data_set, const Matrix *parameter_key) {
     size_t rec_count = data_set.rec_count;
     size_t dims = data_set.field_count;
-    size_t class_count = parameter_key[0].cols;
-
-    double *result_for_class = malloc(class_count*sizeof(double));
-    if (result_for_class == NULL) return;
 
     Matrix input_vector = init_matrix(1, dims);
 
@@ -84,24 +80,9 @@ void naive_bayes_classify_dataset(DataSet data_set, const Matrix *parameter_key)
             input_vector.values[0][i] = data_set.data[record]->values[i];
         }
 
-        for (size_t i = 0; i < class_count; i++) {
-            result_for_class[i] = parameter_key[0].values[0][i] * multi_var_normal_pdf(parameter_key[1+i], parameter_key[1+class_count+i], input_vector);
-        }
-
-        double curr_max = result_for_class[0];
-        int max_index = 0;
-
-        for (size_t i = 1; i < class_count; i++) {
-            if (result_for_class[i] > curr_max) {
-                curr_max = result_for_class[i];
-                max_index = (int)i;
-            }
-        }
-
-        data_set.data[record]->label_num = max_index;
+        data_set.data[record]->label_num = naive_bayes_classify(input_vector, parameter_key);
     }
 
-    free(result_for_class);
     free_matrix(&input_vector);
 
     return;
